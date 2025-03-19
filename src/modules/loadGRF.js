cf2f7596ea706fb2eb281c253f0eefe0eecdf0c2
Niko H. <Sehrentos@users.noreply.github.com>
2025-03-19 12:52:02 +0200

    change fs sync to fs/promises

--- src/modules/loadGRF.js
+++ src/modules/loadGRF.js
@@ -1,20 +1,53 @@
 import { GrfNode } from 'grf-loader';
-import { openSync, readFileSync } from 'fs';
+import { open, readFile } from 'fs/promises';
+
+// TODO: I noticed that when caching grf, the CPU spiked on usage,
+// but its faster for delivery. This part needs some optimisation.
+// Also it did not matter was the cache set before or after the grf.load call.
+// Disabled the grf cache for now.
+
+// // #region caches
+// const cache = {
+//     /** @type {string[]} - `DATA.ini` file cache. */
+//     grfNames: [],
+//     /** @type {{[key: string]: GrfNode}} - GrfNode parsed cache. */
+//     grf: {},
+// };
+// // #endregion
 
 /**
- * Loads a file from the ROBrowser zdata.grf file.
+ * Loads a file from the grf file.
  * @param {string} searchFilename the filename to search for in the grf. eg `'data/clientinfo.xml'`
  * @returns {Promise<Uint8Array>} the content of the file as a Uint8Array.
  */
 export async function loadGRF(searchFilename) {
     const name = searchFilename.slice(1).replace(/\//g, '\\');
-    const grfFiles = parseDataIni();
-    for (const grfFile of grfFiles) {
+    const grfNames = await getDataIni();
+    // if (cache.grfNames.length === 0) {
+    //     cache.grfNames = await getDataIni();
+    // }
+
+    for (const grfFile of grfNames/*cache.grfNames*/) {
+        let fh;
         try {
-            const fd = openSync(`./resources/${grfFile}`, 'r');
-            const grf = new GrfNode(fd);
+            // get cache first
+            // if (cache.grf[grfFile]) {
+            //     // await cache.grf[grfFile].load();
+            //     const { data, error } = await cache.grf[grfFile].getFile(name);
+            //     if (error) {
+            //         throw error;
+            //     }
+            //     return data;
+            // }
+            // no cache
+            // open grf and then set cache
+            fh = await open(`./resources/${grfFile}`, 'r');
+            const grf = new GrfNode(fh.fd);
             // Start parsing the grf.
             await grf.load();
+            // set cached grf for the next call
+            //cache.grf[grfFile] = grf;
+            // list all files for debug
             // grf.files.forEach((entry, path) => {
             //     console.log(path);
             // });
@@ -24,7 +57,11 @@ export async function loadGRF(searchFilename) {
             }
             return data;
         } catch (error) {
-            // file not found
+            // file not found or error
+        } finally {
+            if (fh) {
+                await fh.close();
+            }
         }
     }
 }
@@ -39,11 +76,11 @@ export async function loadGRF(searchFilename) {
  * 3=data.grf
  * ```
  * 
- * @returns {string[]}
+ * @returns {Promise<string[]>}
  */
-function parseDataIni() {
+async function getDataIni() {
     try {
-        const dataIni = readFileSync('./resources/data.ini', 'utf-8');
+        const dataIni = await readFile('./resources/data.ini', 'utf-8');
         const lines = dataIni.split('\n');
         /** @type {{key: string, value: string}[]} */
         const files = [];
